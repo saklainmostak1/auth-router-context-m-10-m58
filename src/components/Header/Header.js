c29a64a9e0afdc6ff40b492c46fc2e3ea7e75005
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-16 00:58:54 +0600

    log in

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="navbar bg-primary text-primary-content">
+                <Link  className="btn btn-ghost normal-case text-xl" to='/'>Awesome Auth</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to='/'>Home</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to='/login'>Log In</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to='/register'>Register</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
